5487a400b21d0d848e2d4f735cda44e9ee3ad194
Saiteja Godi <54551930+SaiTejaGodi@users.noreply.github.com>
2025-05-01 09:10:02 +0200

    Update compute.tf

--- modules/compute.tf
+++ modules/compute.tf
@@ -1,1 +1,26 @@
 # Placeholder for compute module
+resource "aws_instance" "web" {
+  ami                         = "ami-0c55b159cbfafe1f0"
+  instance_type               = "t2.micro"
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.sg_id]
+  iam_instance_profile        = var.iam_instance_profile
+  associate_public_ip_address = true
+  tags = { Name = "WebServer" }
+}
+
+resource "aws_ebs_volume" "web_data" {
+  availability_zone = aws_instance.web.availability_zone
+  size              = 8
+}
+
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = "/dev/xvdf"
+  volume_id   = aws_ebs_volume.web_data.id
+  instance_id = aws_instance.web.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
