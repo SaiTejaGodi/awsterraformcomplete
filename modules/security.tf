7977adfa48ab4ebb33f001b350170b79381c6158
Saiteja Godi <54551930+SaiTejaGodi@users.noreply.github.com>
2025-05-01 09:13:58 +0200

    Update security.tf

--- modules/security.tf
+++ modules/security.tf
@@ -1,1 +1,33 @@
 # Placeholder for security module
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "sg_id" {
+  value = aws_security_group.web_sg.id
+}
+
